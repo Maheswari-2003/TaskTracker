00b01c0148d688975cf4c3fb45cf6fbbd458fea6
Maheswari-2003 <89957091+Maheswari-2003@users.noreply.github.com>
2024-01-25 16:57:43 +0530

    Update SecurityConfig.java

--- com/example/tasktrackerapplication/entity/SecurityConfig.java
+++ com/example/tasktrackerapplication/entity/SecurityConfig.java
@@ -16,18 +16,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserService userDetailsService;
+    private JwtFilter jwtFilter;
 
     @Autowired
-    private JwtFilter jwtFilter;
+    private UserService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -36,9 +33,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+      
+        return new NoOpPass();
     }
-
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -49,15 +46,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/authenticate", "/api/signup").permitAll()
+                .antMatchers("/api/signup","/api/authenticate").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                .exceptionHandling()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.cors();
     }
-
-
 }
